5c3e669e33af8d3141dead1befc2c6132a2123e8
sugaaloop <hennessey.re@gmail.com>
2015-08-10 14:20:05 -0400

    stuff

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,10 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+// 
+router.get('/error', function(req, res, next) {
+  res.render('error', { title: 'Error' });
+});
+
+
 module.exports = router;
