5becca857626097dd267178b5542e2796e0100ea
Brandon Thomas <brandon.thomas@gopetplan.com>
2015-08-11 13:40:36 -0400

    Added some profile data using angular to index.jade

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -2,7 +2,7 @@ var battlenetApp = angular.module('battlenetApp', []);
 
 battlenetApp.controller('DisplayProfileCtrl', function($scope, $http) {
     $http.get("https://us.api.battle.net/sc2/profile/582084/1/Bran/?locale=en_US&apikey=pshyp9tgvegq2wuftgu4pu9femmpgbcj")
-        .success(function(response) {
-            $scope.profile = response;
+        .then(function(response) {
+            $scope.profile = response.data; // success is depricated use .then - need to figure out why displayName drops off when I use .then.
         });
 });
