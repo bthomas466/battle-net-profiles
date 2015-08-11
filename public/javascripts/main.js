dbdea9dd9ca1bacc63266f8bc50f55ec9156f17d
Brandon Thomas <brandon.thomas@gopetplan.com>
2015-08-11 13:44:37 -0400

    Added a catch error to http.get call

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -3,6 +3,9 @@ var battlenetApp = angular.module('battlenetApp', []);
 battlenetApp.controller('DisplayProfileCtrl', function($scope, $http) {
     $http.get("https://us.api.battle.net/sc2/profile/582084/1/Bran/?locale=en_US&apikey=pshyp9tgvegq2wuftgu4pu9femmpgbcj")
         .then(function(response) {
-            $scope.profile = response.data; // success is depricated use .then - need to figure out why displayName drops off when I use .then.
+            $scope.profile = response.data;
+        })
+        .catch(function(response) {
+            console.log('Oh crap, something broke', response.status, response.data);
         });
 });
