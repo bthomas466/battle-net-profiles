567179e29f522187fe192696121cddfa7139c5ff
Brandon Thomas <brandon.thomas@gopetplan.com>
2015-08-13 13:49:50 -0400

    looping through last 25 matches

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,5 +1,6 @@
 var battlenetApp = angular.module('battlenetApp', []);
 
+//Get Profile data
 battlenetApp.controller('DisplayProfileCtrl', function($scope, $http) {
     $http.get("https://us.api.battle.net/sc2/profile/582084/1/Bran/?locale=en_US&apikey=pshyp9tgvegq2wuftgu4pu9femmpgbcj")
         .then(function(response) {
@@ -9,3 +10,14 @@ battlenetApp.controller('DisplayProfileCtrl', function($scope, $http) {
             console.log('Oh crap, something broke', response.status, response.data);
         });
 });
+
+//Get Match History
+battlenetApp.controller('MatchHistoryCtrl', function ($scope, $http) {
+    $http.get("https://us.api.battle.net/sc2/profile/582084/1/Bran/matches?locale=en_US&apikey=pshyp9tgvegq2wuftgu4pu9femmpgbcj")
+        .then(function(response) {
+            $scope.matches = response.data.matches;
+        })
+        .catch(function(response) {
+            console.log('Something broke', response.status, response.data);
+        });
+})
