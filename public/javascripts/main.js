b2cac6c691802c2bdb4ea36ffe6110e3bfd6c7ea
Brandon Thomas <brandon.thomas@gopetplan.com>
2015-08-21 13:36:28 -0400

    Can now enter in battle net ID and user name in form.  On submit data populates.

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,23 +1,42 @@
 var battlenetApp = angular.module('battlenetApp', []);
 
-//Get Profile data
-battlenetApp.controller('DisplayProfileCtrl', function($scope, $http) {
-    $http.get("https://us.api.battle.net/sc2/profile/582084/1/Bran/?locale=en_US&apikey=pshyp9tgvegq2wuftgu4pu9femmpgbcj")
-        .then(function(response) {
-            $scope.profile = response.data;
-        })
-        .catch(function(response) {
-            console.log('Oh crap, something broke', response.status, response.data);
-        });
-});
+//Service
+battlenetApp.factory('idAndName', ['$http', function($http) {
+    return {
+        //get user profile - params: id, name
+        getProfile: function(id, name) {
+            return $http.get("https://us.api.battle.net/sc2/profile/"+ id +"/1/"+ name +"/?locale=en_US&apikey=pshyp9tgvegq2wuftgu4pu9femmpgbcj")
+                .then(function(response) {
+                    return response.data;
+                })
+                .catch(function(response) {
+                    console.log('Something broke', response.status, response.data);
+                });
+        },
+        //get last 25 matches - params: id, name
+        getMatches: function(id, name) {
+            return $http.get("https://us.api.battle.net/sc2/profile/"+ id +"/1/"+ name +"/matches?locale=en_US&apikey=pshyp9tgvegq2wuftgu4pu9femmpgbcj")
+                .then(function(response) {
+                    return response.data.matches;
+                })
+                .catch(function(response) {
+                    console.log('Something broke', response.status, response.data);
+                });
+        }
+    };
+}]);
 
-//Get Match History
-battlenetApp.controller('MatchHistoryCtrl', function ($scope, $http) {
-    $http.get("https://us.api.battle.net/sc2/profile/582084/1/Bran/matches?locale=en_US&apikey=pshyp9tgvegq2wuftgu4pu9femmpgbcj")
-        .then(function(response) {
-            $scope.matches = response.data.matches;
-        })
-        .catch(function(response) {
-            console.log('Something broke', response.status, response.data);
+//Get User information
+battlenetApp.controller('GetUserInformationCtrl', ['$scope', 'idAndName', function($scope, idAndName) {
+    //create submit function to get form data
+    $scope.processForm = function() {
+        idAndName.getProfile($scope.id, $scope.name).then(function(response) {
+            $scope.profile = response;
+            console.log($scope.profile);
         });
-})
+        idAndName.getMatches($scope.id, $scope.name).then(function(response) {
+            $scope.matches = response;
+            console.log($scope.matches);
+        });
+    };
+}]);
